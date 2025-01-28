753cb678e3c0b060d401686c146c86d5c9d20f1b
MalinJGit <malinjohansson92@hotmail.com>
2025-01-28 12:31:55 +0100

    Updated my test as well since some steps were missing

--- cypress/e2e/test.ts
+++ cypress/e2e/test.ts
@@ -4,12 +4,14 @@ Before(() => {
     cy.visit('http://localhost:5173/')
   })
 
-Given('Jag är på hemsidan och ska se en text med "About us" i navbaren', () => {})
+  Given('Jag är på hemsidan och ska se en text med "About us" i navbaren', () => {
+    cy.get('nav').contains('About us').should('be.visible');
+  });
 
 
 When('Jag rör datormusen över navbaren och klickar på texten "About us"', () => {
-    cy.get('nav').contains('About us').should('be.visible');
-})
+    cy.get('nav').contains('About us').click();
+});
 
 Then('Blir då omdirigerad till en ny sida med information om skaparna', () => {
     cy.url().should('include', '/about');
@@ -22,7 +24,6 @@ Then('Jag vill också kunna skapa ett konto eller logga in. Därbör bör login-
   });
 
   Then('Jag vill också kunna gå tillbaka till startsidan via en länk i navbaren', () => {
-    cy.get('navbar').contains('Home').should('be.visible');
-    cy.get('navbar').contains('Home').click();
+    cy.get('nav').contains('Home').should('be.visible').click();
     cy.url().should('eq', 'http://localhost:5173/');
   });
